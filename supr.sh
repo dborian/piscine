44474a9cac2524a19ebc03750119305aa14586b3
Dorian Edme <edmedorian@gmail.com>
2024-01-24 15:06:49 +0100

    je tente des truc

--- supr.sh
+++ supr.sh
@@ -1,5 +1,9 @@
-rm */*/a.out 2>/dev/null
-rm */*.o 2>/dev/null
 rm */*/*.o 2>/dev/null
+rm */*.o 2>/dev/null
+rm *.o 2>/dev/null
+rm a.out 2>/dev/null
+rm */a.out 2>/dev/null
+rm */*/a.out 2>/dev/null
+rm *.a 2>/dev/null
 rm */*.a 2>/dev/null
 rm */*/*.a 2>/dev/null
