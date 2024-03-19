587e07dd71dfdb0787951657da4eaea398f2c99b
dborian <edmedorian@gmail.com>
2024-03-19 20:32:08 +0100

    random things

--- random/memory_game/memory_game.c
+++ random/memory_game/memory_game.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	ft_chartoint(char *str)
+{
+	int i;
+	int r;
+
+	i = 0;
+	r = 0;
+	while(str[i] != '\0')
+	{
+		r = (r - 48) * 10 + str[i];
+		i++;
+	}
+	return(r);
+}
+
+void	ft_file(char *player_name,int score)
+{
+	char filename[10];
+	char actual[10];
+	FILE* fichier = NULL;
+	int answer;
+
+	sprintf(filename,"%s.txt",player_name);
+	fichier = fopen(filename, "r+");
+	answer = 0;
+	if (fichier != NULL)
+	{
+		// l'utilisateur existe
+		fgets(actual, 10, fichier);
+		printf("ton score actuelle est de %d et ton meilleur score est %d\n",score, ft_chartoint(actual));
+	}
+	else
+	{
+		// l'utilisateur n'existe pas
+		printf("cette utilisateur n'existe pas voulez vous le créer ?\n1 (oui)\n2 (non)\n");
+		scanf("%d", &answer);
+		if(answer == 1)
+		{
+			fopen(filename, "a+");
+			fputc('0', fichier);
+			fclose(fichier);
+			ft_file(player_name, score);
+			return;
+		}
+		else
+		{
+			printf("votre score ne sera pas enregistrer");
+		}
+		return;
+	}
+}
 int	ft_is_good(int guess, int nbr, int lives)
 {
 	if(guess <= 100 && guess >= 0)
@@ -50,6 +101,7 @@ void	ft_guess(void)
 		}
 	}
 	printf("you lose\n%s your score is %d\n",player_name, score);
+	ft_file(player_name, score);
 	printf("do you want to play again\n1 (yes)\n2 (no)\n");
 	scanf("%d", &again);
 	if(again == 1)
