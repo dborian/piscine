b84efb71a31d7e043930382514a0a3b081bb5359
dborian <edmedorian@gmail.com>
2024-03-22 14:39:58 +0100

    it work

--- random/memory_game/memory_game.c
+++ random/memory_game/memory_game.c
@@ -30,19 +30,26 @@ void	ft_file(char *player_name,int score)
 	{
 		// l'utilisateur existe
 		fgets(actual, 10, fichier);
-		printf("ton score actuelle est de %d et ton meilleur score est %d\n",score, ft_chartoint(actual));
+		if(atoi(actual) < score)
+		{
+			sprintf(actual, "%d", score);
+			fputs(actual, fichier);	
+			//j'en suis la pls
+		}
+		printf("ton score actuelle est de %d et ton meilleur score est %s\n",score, actual);
 	}
 	else
 	{
 		// l'utilisateur n'existe pas
-		printf("cette utilisateur n'existe pas voulez vous le créer ?\n1 (oui)\n2 (non)\n");
+		printf("l'utilisateur %s n'existe pas, souhaite tu le créer ?\n1 (oui)\n2 (non)\n", player_name);
 		scanf("%d", &answer);
 		if(answer == 1)
 		{
-			fopen(filename, "a+");
-			fputc('0', fichier);
+			fichier = fopen(filename, "a+");
+			sprintf(actual, "%d", score);
+			fputs(actual, fichier);
 			fclose(fichier);
-			ft_file(player_name, score);
+			printf("utilisateur %s créer correctement\n", player_name);
 			return;
 		}
 		else
@@ -72,32 +79,31 @@ int	ft_is_good(int guess, int nbr, int lives)
 	return(lives);
 }
 
-void	ft_guess(void)
+void	ft_guess(char player_name[10], int i, int tab[50])
 {
-	int nbr;
 	int guess;
 	int lives;
 	int again;
 	int score;
-	char player_name[10];
 
-	printf("what's your name\n");
-	scanf("%s", player_name);
-	nbr = rand() % 100;
-	lives = 10;
-	guess = nbr + 1;
-	again = 0;
 	score = 0;
+	lives = 10;
+	guess = tab[score] + 1;
+	again = 0;
 	printf("guess a number\n");
 	while(lives > 0)
 	{
 		scanf("%d", &guess);
-		lives = ft_is_good(guess, nbr, lives);
-		if(guess == nbr)
+		lives = ft_is_good(guess, tab[score], lives);
+		if(guess == tab[score])
 		{
 			score++;
-			nbr = rand() % 100;
 			printf("you guess good\nnow keep upgrading your score\n");
+			if(score >= i)
+			{
+				i++;
+				tab[i] = rand() % 100;
+			}
 		}
 	}
 	printf("you lose\n%s your score is %d\n",player_name, score);
@@ -105,7 +111,7 @@ void	ft_guess(void)
 	printf("do you want to play again\n1 (yes)\n2 (no)\n");
 	scanf("%d", &again);
 	if(again == 1)
-		ft_guess();
+		ft_guess(player_name, i, tab);
 	else
 	{
 		printf("goodbye\n");
@@ -114,9 +120,20 @@ void	ft_guess(void)
 
 }
 
+void	ft_connect(void)
+{
+	int tab[50];
+	char player_name[10];
+
+	tab[0] = rand() % 100;
+	printf("what's your name\n");
+	scanf("%s", player_name);
+	ft_guess(player_name, 0, tab);
+	return;
+}
+
 int	main(void)
 {
-	ft_guess();
-
+	ft_connect();
 	return(0);
 }
